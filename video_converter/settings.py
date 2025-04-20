fe079523a5f0f583539d8875f249d775d00f08c6
Alex <Alex_TNK_BP@mail.ru>
2025-04-20 10:18:45 +0300

    changing the configuration

--- video_converter/settings.py
+++ video_converter/settings.py
@@ -19,12 +19,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#f9d@-$ux381yy&bh7l^s36qp_m&$$*hou#(vq*&640n8%=xlo'
+#SECRET_KEY = 'django-insecure-#f9d@-$ux381yy&bh7l^s36qp_m&$$*hou#(vq*&640n8%=xlo'
+
+SECRET_KEY = str(os.environ["SECRET_KEY"])
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = os.environ.get(
+    "DJANGO_ALLOWED_HOSTS",
+    default='localhost 127.0.0.1 [::1]'
+).split(" ")
 
 # Application definition
 
@@ -73,10 +80,20 @@ WSGI_APPLICATION = 'video_converter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": str(os.environ["DBNAME"]),
+        "USER": str(os.environ["DBUSER"]),
+        "PASSWORD": str(os.environ["DBPASS"]),
+        "HOST": str(os.environ.get("DBHOST", "localhost")),
+        "PORT": str(os.environ.get("DBPORT", "5432")),
     }
 }
 
@@ -112,7 +129,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = str(os.environ["WWW_DIR"]) + '/static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(os.environ["WWW_DIR"]) + '/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
